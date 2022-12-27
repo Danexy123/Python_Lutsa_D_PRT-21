f520ffe2fd3ff54fbf1852832d9b3824831c362a
Danexy <danch2003@yandex.ru>
2022-12-27 23:13:28 +0500

    Update LAB3.py

--- LAB3_pyDatalog/LAB3.py
+++ LAB3_pyDatalog/LAB3.py
@@ -1,18 +1,37 @@
 from pyDatalog import pyDatalog
 import random
 
-pyDatalog.create_terms('X, Z, Y, Sm, div, Average, y, SumRand')
+#регистрация правил
+pyDatalog.create_terms('X, Z, Y, Sm, div, Average, y, SumRand, resultMulti, result')
 
 # Сумма
 (y[X] == sum_(Y, for_each=Y)) <= ((Y._in(range_(X + 1))))
-print(y[999999] == Sm)
+print(y[999] == Sm)
+
+
 
 # Среднее
 (div[X, Y] == Z) <= (X // Y == Z)
-print(div[y[999999], 999999] == Average)
+print(div[y[999], 999] == Average)
+
+
 
 # Медиана
-H = sorted([random.choice(range(99999)) for i in range(100)])
+H = sorted([random.choice(range(999)) for i in range(100)])
 print("Median: ", (H[49] + H[50]) / 2)
+
+
+
+# Произведение
+# for i in range(999):
+#     pyDatalog.assert_fact('number', i)
+#
+# pyDatalog.assert_fact('multiply', (X, Y, result)) <= (X['number'] & Y['number'] & (result == X*Y))
+#
+# result = pyDatalog.ask('result')
+# product = 1
+# for r in result.answers:
+#     product *= r[0]
+# print(product)
 
 
