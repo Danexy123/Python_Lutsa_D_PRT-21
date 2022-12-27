ae9f15e4ff973abbbd6ce58a0204c90af6dc3b2c
Danexy <danch2003@yandex.ru>
2022-12-27 22:49:55 +0500

    Update LAB3.py

--- LAB3_pyDatalog/LAB3.py
+++ LAB3_pyDatalog/LAB3.py
@@ -7,3 +7,8 @@ pyDatalog.create_terms('X, Z, Y, Sm, div, Average, y, SumRand')
 (y[X] == sum_(Y, for_each=Y)) <= ((Y._in(range_(X + 1))))
 print(y[999999] == Sm)
 
+# Среднее
+(div[X, Y] == Z) <= (X // Y == Z)
+print(div[y[999999], 999999] == Average)
+
+
