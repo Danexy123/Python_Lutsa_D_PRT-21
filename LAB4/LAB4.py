d93f33efe45150278839e6a5a0fa6f9ad072511d
Danexy <danch2003@yandex.ru>
2022-12-28 00:09:17 +0500

    Update LAB4.py

--- LAB4/LAB4.py
+++ LAB4/LAB4.py
@@ -3,8 +3,8 @@ import csv
 
 with open("zdrv.csv","r") as file:
   keys = file.readline()
-  reader = csv.reader(file, delimiter=",")
+  reader = csv.reader(file, delimiter=";")
   dataset = [x for x in reader]
-keys = list(keys.split(","))
-town = list(map(lambda x: x[8] == '9,41', dataset))
+keys = list(keys.split(";"))
+town = list(map(lambda x: x[1] == '4,4', dataset))
 print(town.count(True))
